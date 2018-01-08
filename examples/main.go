c8409222c240de87232172f7fe35c1e3dd511319
Radu Matei <matei.radu94@gmail.com>
2018-01-08 18:15:56 +0200

    Update example

--- examples/main.go
+++ examples/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 
 	kube "github.com/radu-matei/kube-exec"
@@ -11,16 +11,23 @@ var kubeconfig = os.Getenv("KUBECONFIG")
 
 func main() {
 
-	cfg := kube.KubeConfig{
+	cfg := kube.Config{
 		Kubeconfig: kubeconfig,
 		Image:      "ubuntu",
-		Name:       "kube-exec",
+		Name:       "kube-exec-attach-req-28",
 		Namespace:  "default",
 	}
 
-	cmd := kube.Command(cfg, "echo", "Hello, Universe!")
+	cmd := kube.Command(cfg, "/bin/sh", "-c", "while true; do echo hello; sleep .5; done")
 	err := cmd.Start()
 	if err != nil {
-		fmt.Printf("%v", err)
+		log.Fatalf("cannot start command: %v", err)
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("cannot wait command: %v", err)
 	}
 }
