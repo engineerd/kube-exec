70c83337c1ba3694c55bd68ce4e879cd5e944468
Radu Matei <matei.radu94@gmail.com>
2018-01-09 02:49:44 +0200

    Add stdin example

--- examples/main.go
+++ examples/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 
@@ -14,15 +15,29 @@ func main() {
 	cfg := kube.Config{
 		Kubeconfig: kubeconfig,
 		Image:      "ubuntu",
-		Name:       "kube-exec-attach-req-28",
+		Name:       "kube-attach",
 		Namespace:  "default",
 	}
 
-	cmd := kube.Command(cfg, "/bin/sh", "-c", "while true; do echo hello; sleep .5; done")
+	cmd := kube.Command(cfg, "/bin/sh", "-c", "while true; read test; do echo hello; echo $test; sleep .5; done")
+	//cmd := kube.Command(cfg, "/bin/sh", "-c", "while true; do echo hello; sleep .5; done")
 	err := cmd.Start()
 	if err != nil {
 		log.Fatalf("cannot start command: %v", err)
 	}
+
+	w, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatalf("cannot get pipe to stdin: %v", err)
+	}
+
+	go func() {
+		defer w.Close()
+		_, err = io.Copy(w, os.Stdin)
+		if err != nil {
+			log.Fatalf("cannot copy from stdin: %v", err)
+		}
+	}()
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -30,4 +45,5 @@ func main() {
 	if err != nil {
 		log.Fatalf("cannot wait command: %v", err)
 	}
+
 }
